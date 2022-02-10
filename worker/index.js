bb5b3ee9103b5bc199dd2480ccb0137d2d2304dd
Ahmed Riad <ahmed.riad.elashrey@gmail.com>
2022-02-10 15:22:29 +0300

    :ambulance: fix timeout error in worker

--- worker/index.js
+++ worker/index.js
@@ -1,21 +1,22 @@
-import { createEventHandler } from '@remix-run/cloudflare-workers'
+import {
+  createRequestHandler,
+  handleAsset,
+} from '@remix-run/cloudflare-workers'
 import * as build from '../build'
 
-const handler = createEventHandler({ build })
+const handler = createRequestHandler({ build })
 
 const handleRedirect = async event => {
+  const asset = await handleAsset(event)
+  if (asset) return asset
+
   const path = new URL(event.request.url).pathname
   const location = await REDIRECTS.get(path)
-
   if (location) return Response.redirect(location, 307)
 
-  return null
+  return handler(event)
 }
 
-const handleRequest = async event => {
-  const redirect = await handleRedirect(event)
-  if (redirect) return event.respondWith(redirect)
-  handler(event)
-}
-
-addEventListener('fetch', handleRequest)
+addEventListener('fetch', event => {
+  event.respondWith(handleRedirect(event))
+})
