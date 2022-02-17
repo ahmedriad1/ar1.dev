41235bcf8eb238580e8ea60c702b3ed22f07edbb
Ahmed Riad <ahmed.riad.elashrey@gmail.com>
2022-02-17 21:43:28 +0300

    :sparkles: add social links in footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,11 +1,33 @@
-import { HeartIcon } from '@heroicons/react/outline'
+import { HeartIcon, MailIcon } from '@heroicons/react/outline'
 import Container from './Container'
+import { GithubIcon, TwitterIcon } from './Icons'
+import type { IconProps } from './Icons'
+
+interface SocialLinksProps {
+  href: string
+  icon: React.ElementType<IconProps>
+}
+
+const SocialLink = ({ href, icon: Icon }: SocialLinksProps) => (
+  <a
+    href={href}
+    target="_blank"
+    className="text-gray-500 hover:text-black dark:text-primary-lighter dark:hover:text-white transition-colors"
+  >
+    {<Icon className="w-8 h-8" />}
+  </a>
+)
 
 export default function Footer() {
   return (
     <footer className="py-16 border-t border-gray-200 dark:border-gray-600">
       <Container className="w-full">
-        <div className="flex justify-between items-center flex-col md:items-end md:flex-row text-lg">
+        <div className="flex items-center justify-center md:justify-start text-lg space-x-4">
+          <SocialLink href="/gh" icon={GithubIcon} />
+          <SocialLink href="/twitter" icon={TwitterIcon} />
+          <SocialLink href="mailto:ahmed@ar1.dev" icon={MailIcon} />
+        </div>
+        <div className="flex justify-between items-center flex-col md:items-end md:flex-row text-lg mt-8">
           <p className="flex items-center">
             Made with <HeartIcon className="w-7 h-7 text-accent mx-2" /> by
             Ahmed
